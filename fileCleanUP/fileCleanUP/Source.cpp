5a269265fe14927836b3edec1c675f9a2b0c8961
Candelario Daniel Eguia <candelario.eguia@gmail.com>
2016-03-04 22:12:22 -0800

    Initialize Source.cpp with header and #includes

---
+++ fileCleanUP/fileCleanUP/Source.cpp
@@ -0,0 +1,15 @@
+//---------------------------------------------------------------
+//          Name		Candelario D. Eguia
+//			Name        Jeremy Driesler
+//          Course		CMPS-455
+//          Project		No.4
+//          Due date	Mar. 11, 2015
+//          Professor	Ray Ahmadnia
+//
+// This program will:
+//       Clean up and copy a text file
+//------------------------------------------------------------------
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <cstring>
