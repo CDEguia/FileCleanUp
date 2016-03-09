ed6b8dff246da505a86ede897fe77a53fcdf5b59
Candelario Daniel Eguia <candelario.eguia@gmail.com>
2016-03-09 07:29:04 -0800

    added // detection

--- fileCleanUP/fileCleanUP/Source.cpp
+++ fileCleanUP/fileCleanUP/Source.cpp
@@ -21,19 +21,26 @@ int main() {
 	char reservedWords[4][10] = { "cout<<","for","int","while" };
 	char special[10][3] = { "+" , "=" , "*" , "-" , ";" , "(" , ")" , "<=", "//", "," };
 	string w;						//hold line
-	fstream inp;					//base file
+	fstream inp, outp;				//base file
 	
 	inp.open("data.txt", ios::in);	//open the file that includes the strings to check
-	
+
 	getline(inp, w);				//get the first word in the file
 	
-	while (!inp.eof())				//loop through the words in the text file
+	while (!inp.eof())				//loop through the lines in the text file
 	{
-		
-		getline(inp, w);			//get the next word
+		int i = 0;
+		while (w[i] != '\n')		//loops through character in each line
+		{
+			if (w[i] == '/' && w[i + 1] == '/') break; //skips any commented code
+
+			i++;
+		}
+		getline(inp, w);			//get the next line
 	}
 	//close the file stream
 	inp.close();
+	outp.close();
 	//terminate the program
 	system("pause");
 	return 0;
